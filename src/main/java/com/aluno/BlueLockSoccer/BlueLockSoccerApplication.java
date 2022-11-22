327b2cff2808b93b712eb5e8934984e0766ef5da
Rodrigo A. Silva <rgbrasil2@gmail.com>
2022-11-22 18:53:05 -0300

    First commit

---
+++ src/main/java/com/aluno/BlueLockSoccer/BlueLockSoccerApplication.java
@@ -0,0 +1,13 @@
+package com.aluno.BlueLockSoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueLockSoccerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueLockSoccerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluno/BlueLockSoccer/BlueLockSoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluno.BlueLockSoccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueLockSoccerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
